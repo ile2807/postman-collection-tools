3657609bfe11f5fa488c017c03b99a1665ebe749
Ilija Trajkoski <ile2807@gmail.com>
2021-05-14 11:09:42 +0200

    Inital add

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# postman-collection-tools
-Postman collections manipulation tools
+# Jackal tools
+Jackal tools is an utility application that manipulates `Postman collections`.
+
+This application combines all collection variables from postman collections located in the `source` folder and adds into the `target` collection
+
+## How to use it
+
+To run the application just execute the application with the correct command line arguments
+
+### As npm module
+
+```bash
+npm i jackal-tools -g
+
+jackal -f [source-folder] -s [start-collection] -o [output-collection]
+```
+
+### As node application from source
+
+```bash
+node index.js -f [source-folder] -s [start-collection] -o [output-collection]
+```
+
+> NOTE: start-collection argument is optional, if not specified a new postman collection will be created
+
+
+After the execution
+
+```Bash
+      _                  _              _ 
+     | |   __ _    ___  | | __   __ _  | |
+  _  | |  / _` |  / __| | |/ /  / _` | | |
+ | |_| | | (_| | | (__  |   <  | (_| | | |
+  \___/   \__,_|  \___| |_|\_\  \__,_| |_|
+
+Source collections folder > ./examples
+Start collection to be upgraded > blank.json
+Target collection > test.json
+---------------------------------------------------------------------
+Processing source file: Sample With variables 2.postman_collection.json
+Processing source file: Sample With variables.postman_collection.json  
+---------------------------------------------------------------------  
+Merge fiinished successfully
+---------------------------------------------------------------------   
+```
+and the Output collection will be populated with the aggregated variables
+
+## NOTES
+- The application will only append Collection Variables in the Output collection 
+- You can use the Output collection as a Start collection, in this case only the new variables will be added and the Start collection **will be overwritten**
+- Source of Collection Variables are all the collections located in the Source folder
+- Duplicate (name and value) variables will not be added in the collection multiple times
+- If values are different in two same named variables then both will be added to the Output collection
+- Empty variables are not added 
---
+++ index.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+const stripJSONComments = require('strip-json-comments');
+const Collection = require('postman-collection').Collection;
+const { exit } = require('process');
+const args = require('minimist')(process.argv.slice(2));
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+var sourceCollectionName = args.s;
+const targetCollectionName = args.o;
+const collectionsFoleder = args.f;
+const command = args.c;
+
+const line = () => console.log(chalk.gray("---------------------------------------------------------------------"));
+
+clear();
+console.log(
+    chalk.yellow(
+        figlet.textSync('Jackal', { horizontalLayout: 'full' })
+    )
+);
+if (args.h) {
+    help();
+}
+
+validateParameters();
+
+if (sourceCollectionName == undefined) {
+    sourceCollectionName = "blank.json"
+}
+
+console.log('Source collections folder > ' + chalk.cyan(collectionsFoleder));
+console.log('Start collection > ' + chalk.cyan(sourceCollectionName));
+console.log('Target collection > ' + chalk.cyan(targetCollectionName));
+line();
+
+var targetCollection = new Collection(JSON.parse(fs.readFileSync(sourceCollectionName).toString()));
+
+fs.readdir(collectionsFoleder, (error, files) => {
+    if (error) return console.log(error);
+    files.forEach(file => {
+        //Reading all the collection files
+        var collection = new Collection(JSON.parse(stripJSONComments(fs.readFileSync(collectionsFoleder + "/" + file).toString())));
+        console.log("Processing source file: " + chalk.yellow(file))
+        collection.variables.each(variable => {
+            //Avoiding duplicates
+            if (!targetCollection.variables.find(variable)) {
+                //Avoiding empty variables
+                if (variable.value !== "") {
+                    targetCollection.variables.append(JSON.parse(JSON.stringify(variable)));
+                }
+            }
+        })
+    });
+    let data = JSON.stringify(targetCollection.toJSON(), null, "\t");
+    //Writing the data
+    fs.writeFile(targetCollectionName, data, function (err) {
+        if (err) return console.log(err);
+        line();
+        console.log(
+            chalk.green.bold("Merge fiinished successfully")
+        );
+        line();
+    });
+});
+
+function help() {
+    console.log(
+        chalk.blue.bold("Welcome to Postman collections tools, this is how to use the application")
+    );
+    line();
+    console.log(
+        chalk.yellow("run jackal command with the following parameters")
+    );
+    console.log(
+        chalk.cyan("-s [source-collection.json] (optional)") + " This is the starting point file to enrich with the end result variables, if not present a blank collection is created and upgraded with the variables"
+    );
+    console.log(
+        chalk.cyan("-f [source-folder] (mandatory)") + " This is a path to the folder that contains all source collections from where the application will get the collection variables"
+    );
+    console.log(
+        chalk.cyan("-o [output-collection.json] (mandatory)") + " This is a filename where the result collection will be written"
+    );
+    line();
+    console.log(
+        chalk.gray.bold("Example:") + " jackal -f ./examples -o result.json"
+    );
+    line();
+    exit();
+}
+
+function validateParameters() {
+    if (collectionsFoleder == undefined) {
+        console.log(
+            chalk.red.bold("Parameters error")
+        );
+        line();
+        console.log("You must provide source folder with existing collections, use the -s option followed by the source folder path, or -h to see help");
+        line();
+        exit();
+    }
+    if (targetCollectionName == undefined) {
+        console.log(
+            chalk.red.bold("Parameters error")
+        );
+        line();
+        console.log("You must provide target file, use -f option followed by the output file path, or -h to see help");
+        line();
+        exit();
+    }
+}
+
